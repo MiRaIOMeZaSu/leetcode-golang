e4da5c096b600a53796e41238092aff4599ed9fd
miraiomezasu <379297266@qq.com>
2021-10-26 09:58:44 +0800

    496: done

--- common/496_nextGreaterElement/main.go
+++ common/496_nextGreaterElement/main.go
@@ -1,25 +1,34 @@
-package nextgreaterelement
+package main
 
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
 	// 使用单调栈
 	size := len(nums1)
 	border := len(nums2)
+	var dict map[int]int = make(map[int]int)
 	var stack []int
 	stack = append(stack, nums2[border-1])
-	for i := border - 2; i >= size; i-- {
+	dict[nums2[border-1]] = -1
+	for i := border - 2; i >= 0; i-- {
 		for len(stack) != 0 && stack[len(stack)-1] < nums2[i] {
-			stack = stack[:1]
+			stack = stack[:len(stack)-1]
+		}
+		if len(stack) == 0 {
+			dict[nums2[i]] = -1
+		} else {
+			dict[nums2[i]] = stack[len(stack)-1]
 		}
 		if len(stack) == 0 || stack[len(stack)-1] > nums2[i] {
 			stack = append(stack, nums2[i])
 		}
 	}
-	for i := size - 1; i >= 0; i-- {
-		curr := nums1[i]
-		for curr >= stack[len(stack)-1] {
-
-		}
+	for i := 0; i < size; i++ {
+		nums1[i] = dict[nums1[i]]
 	}
-
 	return nums1
+}
+
+func main() {
+	var num1 = []int{1, 3, 5, 2, 4}
+	var num2 = []int{6, 5, 4, 3, 2, 1, 7}
+	nextGreaterElement(num1, num2)
 }
