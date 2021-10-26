0af0c5fec46e8069fb2c75414047a9428d8f7a99
miraiomezasu <379297266@qq.com>
2021-10-26 13:03:31 +0800

    556: done

--- common/556_nextGreaterElement/main.go
+++ common/556_nextGreaterElement/main.go
@@ -8,21 +8,45 @@ const (
 
 func nextGreaterElement(n int) int {
 	// 使用排序
-	var nums []int
+	// 只需要使得靠后的某位的值比原来大
+	nums := sort.IntSlice{}
 	curr := n
-	for curr > 0 {
-		nums = append(nums, curr%10)
-		curr /= 10
-	}
-	arr := sort.IntSlice{}
-	arr = append(arr, nums...)
-	sort.Sort(arr)
 	var ans int
-	ans = 0
-	for i := len(arr) - 1; i >= 0; i-- {
-		ans *= 10
-		ans += arr[i]
+	ans = -1
+	max := 0
+	index := 0
+	currIndex := 0
+	for curr > 0 {
+		num := curr % 10
+		if max > num {
+			// 可以开始替换
+			min := max
+			for i := 0; i < len(nums); i++ {
+				if nums[i] > num && nums[i] < min {
+					min = nums[i]
+					index = i
+				}
+			}
+			nums = append(nums[:index], nums[index+1:]...)
+			nums = append(nums, num)
+			ans = curr / 10
+			ans *= 10
+			ans += min
+			sort.Sort(nums)
+			for i := 0; i < len(nums); i++ {
+				ans *= 10
+				ans += nums[i]
+			}
+			break
+		} else {
+			max = num
+			index = currIndex
+		}
+		nums = append(nums, num)
+		curr /= 10
+		currIndex++
 	}
+
 	if ans == n || ans > INT_MAX {
 		return -1
 	}
