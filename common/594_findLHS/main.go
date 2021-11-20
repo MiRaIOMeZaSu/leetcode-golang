1c84c6953d78f857eee8997bc716ad46566c5dd4
miraiomezasu <379297266@qq.com>
2021-11-20 11:03:27 +0800

    594: map

--- common/594_findLHS/main.go
+++ common/594_findLHS/main.go
@@ -1,36 +1,28 @@
 package main
 
 func max(a, b int) int {
-	if a < b {
-		return b
-	}
-	return a
-}
-
-func min(a, b int) int {
-	if a < b {
+	if a > b {
 		return a
 	}
 	return b
 }
 
 func findLHS(nums []int) int {
-	// 使用窗口法?
-	left := 0
-	right := 1
+	// 计算相邻的两个数
 	ans := 0
-	min := min(nums[left], nums[right])
-	max := max(nums[left], nums[right])
-	for right < len(nums) && left <= right {
-		if max-min == 1 {
-			ans = right - left + 1
-			right++
-		} else {
-			left++
-		}
-		if left == right {
-			right++
+	var dict map[int]int
+	dict = make(map[int]int)
+	for _, val := range nums {
+		dict[val]++
+	}
+	for k, v := range dict {
+		if dict[k-1] != 0 {
+			ans = max(ans, dict[k-1]+v)
 		}
 	}
 	return ans
+}
+
+func main() {
+	findLHS([]int{1, 2, 3, 4, 5})
 }
