bb82879058f7e6000c24fac5a669befe16a66925
miraiomezasu <379297266@qq.com>
2021-12-02 20:36:58 +0800

    506: done

--- common/506_findRelativeRanks/main.go
+++ common/506_findRelativeRanks/main.go
@@ -5,45 +5,29 @@ import (
 	"strconv"
 )
 
-type element struct {
-	val   int
-	index int
-}
-type elements []element
-
-func (s elements) Len() int {
-	return len(s)
-}
-func (s elements) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-func (s elements) Less(i, j int) bool {
-	return s[j].val > s[i].val
-}
-
 func findRelativeRanks(score []int) []string {
 	// 先排序
 	// 通过map插入顺序
 	medals := []string{"Gold Medal", "Silver Medal", "Bronze Medal"}
 	size := len(score)
-	sortedContainer := []element{}
+	sortedContainer := sort.IntSlice{}
 	for i := 0; i < size; i++ {
-		sortedContainer = append(sortedContainer, element{score[i], i})
+		sortedContainer = append(sortedContainer, score[i])
 	}
-	sort.Sort(elements(sortedContainer))
+	sort.Sort(sortedContainer)
 	var dict map[int]int
 	dict = make(map[int]int)
 	for i := 0; i < size; i++ {
-		dict[sortedContainer[i].val] = sortedContainer[i].index
+		dict[sortedContainer[i]] = size - i
 	}
 	// 开始输出
 	ans := []string{}
 	for i := 0; i < size; i++ {
 		rank := dict[score[i]]
-		if rank >= 3 {
-			ans = append(ans, strconv.Itoa(rank+1))
+		if rank > 3 {
+			ans = append(ans, strconv.Itoa(rank))
 		} else {
-			ans = append(ans, medals[rank])
+			ans = append(ans, medals[rank-1])
 		}
 	}
 	return ans
