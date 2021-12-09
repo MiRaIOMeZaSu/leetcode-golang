a0adeb610ec4dab18b588e078cd4d8ff3e1c8d85
miraiomezasu <379297266@qq.com>
2021-12-09 22:44:51 +0800

    794: done

--- common/794_validTicTacToe/main.go
+++ common/794_validTicTacToe/main.go
@@ -10,7 +10,7 @@ func validTicTacToe(board []string) bool {
 		for j := 0; j < 3; j++ {
 			if board[i][j] == 'O' {
 				countO++
-			} else {
+			} else if board[i][j] == 'X' {
 				countX++
 			}
 		}
@@ -20,30 +20,38 @@ func validTicTacToe(board []string) bool {
 	}
 	// 此时数量已经满足
 	// 遍历所有成功的手段
-	flag := ""
+	var dict map[string]bool
+	dict = make(map[string]bool)
+	dict["X"] = false
+	dict["O"] = false
 	for i := 0; i < 3; i++ {
 		if board[i][0] == board[i][1] && board[i][2] == board[i][1] {
-			flag += string(board[i][0])
+			dict[string(board[i][0])] = true
 		}
 		if board[0][i] == board[1][i] && board[1][i] == board[2][i] {
-			flag += string(board[0][i])
+			dict[string(board[0][i])] = true
 		}
 	}
 	if board[0][0] == board[1][1] && board[1][1] == board[2][2] {
-		flag += string(board[0][0])
+		dict[string(board[0][0])] = true
 	}
-	if board[2][0] == board[1][1] && board[1][1] == board[2][0] {
-		flag += string(board[1][1])
+	if board[0][2] == board[1][1] && board[1][1] == board[2][0] {
+		dict[string(board[1][1])] = true
 	}
-	if len(flag) > 1 {
+	if dict["X"] && dict["O"] {
+		// 只可能是2
 		return false
 	}
-	if len(flag) == 1 {
-		if flag == "X" {
+	if dict["X"] || dict["O"] {
+		if dict["X"] {
 			return countX-countO == 1
 		} else {
 			return countX == countO
 		}
 	}
 	return true
+}
+
+func main() {
+	validTicTacToe([]string{"   ", "   ", "   "})
 }
