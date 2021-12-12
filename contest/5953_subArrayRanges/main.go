ff9856bd9dc44eabf9a4501fdcfb4b292dc5762c
miraiomezasu <379297266@qq.com>
2021-12-12 12:21:20 +0800

    5953: done

--- contest/5953_subArrayRanges/main.go
+++ contest/5953_subArrayRanges/main.go
@@ -1,39 +1,31 @@
 package main
 
-import "sort"
-
-func queue(smol, big int) int {
-	if big-smol > smol {
-		smol = big - smol
+func max(a, b int) int {
+	if a > b {
+		return a
 	}
-	if smol == big {
-		return 1
+	return b
+}
+func min(a, b int) int {
+	if a > b {
+		return b
 	}
-	ans := 1
-	for i := big; i > smol; i-- {
-		ans *= i
-	}
-	ans /= 2
-	return ans
+	return a
 }
 
 func subArrayRanges(nums []int) int64 {
 	// 重点是所有的子数组
 	// 忽略长度为1的子数组
 	size := len(nums)
-	sorted := sort.IntSlice{}
-	sorted = append(sorted, nums...)
-	sort.Sort(sorted)
 	ans := 0
 	for i := 0; i < size; i++ {
+		minNum := nums[i]
+		maxNum := nums[i]
 		for j := i + 1; j < size; j++ {
+			minNum = min(minNum, nums[j])
+			maxNum = max(maxNum, nums[j])
 			// 寻找两个区间内数字数量,进行排列组合
-			midCount := j - i - 1
-			count := 0
-			for i := 0; i <= midCount; i++ {
-				count += queue(i, midCount)
-			}
-			ans += count * (sorted[j] - sorted[i])
+			ans += maxNum - minNum
 		}
 	}
 	return int64(ans)
