6c66638482dd2a1641572c005b9332b072e383bf
miraiomezasu <379297266@qq.com>
2021-12-31 22:34:46 +0800

    5963&5964: done

---
+++ contest/5963_isSameAfterReversals/main.go
@@ -0,0 +1,11 @@
+package main
+
+func isSameAfterReversals(num int) bool {
+	if num == 0 {
+		return true
+	}
+	if num%10 == 0 {
+		return false
+	}
+	return true
+}
--- contest/5964_executeInstructions/main.go
+++ contest/5964_executeInstructions/main.go
@@ -3,40 +3,49 @@ package main
 func executeInstructions(n int, startPos []int, s string) []int {
 	ans := []int{}
 	size := len(s)
-	for i := 0; i < size; i++ {
-		ans = append(ans, 0)
-	}
 	// 存储在某个位置时可以执行的数量?
-	// var dict map[int]string
 	currPos := []int{}
 
-	// dict = make(map[int]string)
-	for i := size - 1; i >= 0; i-- {
+	for i := 0; i < size; i++ {
 		currPos = []int{}
 		currPos = append(currPos, startPos...)
 		temp := 0
 		for j := i; j < size; j++ {
-			if s[j] == 'U' && currPos[1] > 0 {
-				currPos[1]--
-				temp++
-			} else if s[j] == 'D' && currPos[1] != n-1 {
-				currPos[1]++
-				temp++
-			} else if s[j] == 'L' && currPos[0] > 0 {
-				currPos[0]--
-				temp++
-			} else if s[j] == 'R' && currPos[0] != n-1 {
-				currPos[0]++
-				temp++
-			}
-			if currPos[0] == startPos[0] && currPos[1] == startPos[1] {
-				ans[i] = temp + ans[j]
-				break
+			if s[j] == 'U' {
+				if currPos[0] > 0 {
+					currPos[0]--
+					temp++
+				} else {
+					break
+				}
+			} else if s[j] == 'D' {
+				if currPos[0] != n-1 {
+					currPos[0]++
+					temp++
+				} else {
+					break
+				}
+			} else if s[j] == 'L' {
+				if currPos[1] > 0 {
+					currPos[1]--
+					temp++
+				} else {
+					break
+				}
+			} else if s[j] == 'R' {
+				if currPos[1] != n-1 {
+					currPos[1]++
+					temp++
+				} else {
+					break
+				}
 			}
 		}
-		if ans[i] == 0 {
-			ans[i] = temp
-		}
+		ans = append(ans, temp)
 	}
 	return ans
+}
+
+func main() {
+	executeInstructions(3, []int{0, 1}, "RRDDLU")
 }
